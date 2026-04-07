8a61ae529c3efac320d146279219994d9c6749ff
PR <132150362+Prajjwal-Rimal@users.noreply.github.com>
2026-04-07 23:39:56 +0545

    modification to the boot file
    1. added constants to make the code more redable, and easier to type
    2. replace redundant messsage and new lines to reduce the line of code
    3. changed the bl register to bx register this was throwing the byte
       data exceeds bounds error because i was trying to store a value that
       could not fit in a 8 bit register
    4.changed the leables and removed the newline that came before the jmp
    process

--- code/Bootloader/boot.asm
+++ code/Bootloader/boot.asm
@@ -1,8 +1,16 @@
 org 0x7c00                                              ; starting memory address
+
+; CONSTANTS 
+    newline equ 0x0a
+    teletype_function equ 0x0e
+    printscreen equ 0x10 
+
+
 start:                                                  ; start lable
     cli                                                 ; clearing interrupts
     xor ax,ax                                           ; setting ax operation to 0 via xor operation
     mov ds,ax                                           ; setting the data stack to 0
+    mov es,ax                                           ; setting the extra segment to 0
     mov ss,ax                                           ; setting the stack segment to 0
     mov sp,0x7c00                                       ; setting the stack pointer to the starting language
     sti                                                 ; restarting the interrupts
@@ -15,71 +23,45 @@ print_loop1:                                            ; loop to print the firs
     lodsb                                               ; load the byte from the si to al register then move to the next one
     cmp al,0                                            ; comparing if al=0 
     je new_line                                         ; if al=0 then go to the new line lable
-    mov ah,0x0e                                         ; bios teletype function loaded into the ah register
-    int 0x10                                            ; interrupt to print to the screen
+    mov ah,teletype_function                            ; bios teletype function loaded into the ah register
+    int printscreen                                     ; interrupt to print to the screen
     jmp print_loop1                                     ; repeat until 0 is encountered
 
 ; NEW LINE
 new_line:                                               ; to print the newline
-    mov al,0x0a                                         ; ascii value for line feed fuinction: linebereakl to print the message in another line
-    mov ah,0x0e                                         ; bios teletype function
-    int 0x10                                            ; printing to the screen
+    mov al,newline                                      ; ascii value for line feed fuinction: linebereakl to print the message in another line
+    mov ah,teletype_function                            ; bios teletype function
+    int printscreen                                     ; printing to the screen
     mov al,0x0d                                         ; ascii value for the carriage return: ensures that the cursor is set to the beginning of the new line
-    mov ah,0x0e                                         ; bios teletype function
-    int 0x10                                            ; usdsed to print to the screen
+    mov ah,teletype_function                            ; bios teletype function
+    int printscreen                                     ; usdsed to print to the screen
 
-    mov si,stage2msg                                    ; setting the source index to the second message
-
-; LOOP TO PRINT THE SECOND MESSGAE
-print_loop2:                                            ; loop to print the second messgae
-    lodsb                                               ; loading from si into al and moving to the nect itrem
-    cmp al,0                                            ; comparing the values of the al register with 0
-    je new_line_2                                       ; jumping to new line
-    mov ah,0x0e                                         ; bios teletype function
-    int 0x10                                            ; to p[rint ht evalue in the al reguister to the screen
-    jmp print_loop2                                     ; repeat until the condition is met
-
-; NEW LINE
-new_line_2:                                             ; to print the newline
-    mov al,0x0a                                         ; ascii value for line feed fuinction: linebereakl to print the message in another line
-    mov ah,0x0e                                         ; bios teletype function
-    int 0x10                                            ; printing to the screen
-    mov al,0x0d                                         ; ascii value for the carriage return: ensures that the cursor is set to the beginning of the new line
-    mov ah,0x0e                                         ; bios teletype function
-    int 0x10                                            ; usdsed to print to the screen
-
-
+; reading stage 2 from the disk
     mov ah,0x02
     mov al,8
     mov ch,0
     mov cl,2
     mov dh,0
     mov dl,0x00
-    mov bl,0x8000
+    mov bx,0x8000
     int 0x13
-    jc loop_exit  
+    jc bootloader_loop  
 
 
-    mov si, loadVerificationMessage
+    mov si, stage2msg
 
 ; LOOP TO PRINT THE VERIFICATION MESSGAE
-print_loop3:                                            ; loop to print the second messgae
+print_loop2:                                            ; loop to print the second messgae
     lodsb                                               ; loading from si into al and moving to the nect itrem
     cmp al,0                                            ; comparing the values of the al register with 0
-    je jump_stage2                                       ; jumping to new line
-    mov ah,0x0e                                         ; bios teletype function
-    int 0x10                                            ; to p[rint ht evalue in the al reguister to the screen
-    jmp print_loop3                                     ; repeat until the condition is met
+    je stage2_jump                                      ; jumping to new line
+    mov ah,teletype_function                            ; bios teletype function
+    int printscreen                                     ; to p[rint ht evalue in the al reguister to the screen
+    jmp print_loop2                                     ; repeat until the condition is met
 
 
-new_line_stage2_jump:
-    mov al,0x0a                                         ; line feeder/ new linw
-    mov ah,0x0e                                         ; bios teletype function
-    int 0x10                                            ; print interrupt
-    mov al,0x0d                                         ; carriage return
-    mov ah,0x0e                                         ; BIOSTELETYPE FUNCTION
-    int 0x10                                            ; interrupt to print value to he screen
-    jmp 0x0000:0x8000                                   ; actual jump segment offset memory location                                        ; usdsed to print to the screen
+stage2_jump:
+    jmp 0x0000:0x8000                                   ; actual jump segment offset memory location
 
 
 ; LOOP TO KEEP THE BOOTLOADER RUNNING 
@@ -87,12 +69,7 @@ bootloader_loop:                                        ; bootloader loop
     jmp $                                               ; jumping bacck to the current memory address i.e the bootloader_loop
 
     stage1msg db "STAGE 1 BOOTLOADER LOADED",0          ; the first messgae to print to the screen
-    stage2msg db "SWITCHING TO STAGE 2",0               ; the second message to display on the screen
-    loadVerificationMessage db "STAGE 2 LOADED.", 0     ; VERIFICATION MESSGAE AFTER STAGE 2 DISKIS read
+    stage2msg db "STAGE 2 LOADED.", 0                   ; VERIFICATION MESSGAE AFTER STAGE 2 DISKIS read
 
     times 510 - ( $- $$ ) db 0                          ; setting the remaining bits of the first stage to 0
     dw 0xaa55                                           ; final verification boot signature
-
-;-------------------------------------------------------------------------------------------------------------------------------------------------------------------
-;COMPLETION OF THE FIST STAGE
-;-------------------------------------------------------------------------------------------------------------------------------------------------------------------
