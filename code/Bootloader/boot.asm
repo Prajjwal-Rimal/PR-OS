173d4c59638242ec5cbcba4fefcc7343522bd043
PR <132150362+Prajjwal-Rimal@users.noreply.github.com>
2026-04-08 00:23:17 +0545

    Finalized the first stage of the bootloader Improved redability, comments, and formatting

--- code/Bootloader/boot.asm
+++ code/Bootloader/boot.asm
@@ -2,61 +2,62 @@ org 0x7c00                                              ; starting memory addres
 
 ; CONSTANTS 
     newline equ 0x0a
+    carriageReturn equ 0x0d 
     teletype_function equ 0x0e
-    printscreen equ 0x10 
+    printInterrupt equ 0x10 
 
 
 start:                                                  ; start lable
     cli                                                 ; clearing interrupts
-    xor ax,ax                                           ; setting ax operation to 0 via xor operation
-    mov ds,ax                                           ; setting the data stack to 0
+    xor ax,ax                                           ; setting ax operation to 0, using xor save a byte
+    mov ds,ax                                           ; setting the data segment to 0
     mov es,ax                                           ; setting the extra segment to 0
     mov ss,ax                                           ; setting the stack segment to 0
-    mov sp,0x7c00                                       ; setting the stack pointer to the starting language
+    mov sp,0x7c00                                       ; setting the stack pointer to the starting address
     sti                                                 ; restarting the interrupts
     
     mov si,stage1msg                                    ; moving the source index to the first message
 
 
-; LOOP TO PRINT THE FIRST MESSGAE
-print_loop1:                                            ; loop to print the first messgae on the screen
+; LOOP TO PRINT THE FIRST message
+print_loop1:                                            ; loop to print the first message on the screen
     lodsb                                               ; load the byte from the si to al register then move to the next one
     cmp al,0                                            ; comparing if al=0 
-    je new_line                                         ; if al=0 then go to the new line lable
+    je new_line                                         ; if al=0 then go to the new line label
     mov ah,teletype_function                            ; bios teletype function loaded into the ah register
-    int printscreen                                     ; interrupt to print to the screen
+    int printInterrupt                                  ; interrupt to print to the screen
     jmp print_loop1                                     ; repeat until 0 is encountered
 
 ; NEW LINE
 new_line:                                               ; to print the newline
-    mov al,newline                                      ; ascii value for line feed fuinction: linebereakl to print the message in another line
+    mov al,newline                                      ; ascii value for line feed function: linebereak to print the message in another line
     mov ah,teletype_function                            ; bios teletype function
-    int printscreen                                     ; printing to the screen
-    mov al,0x0d                                         ; ascii value for the carriage return: ensures that the cursor is set to the beginning of the new line
+    int printInterrupt                                  ; printing to the screen
+    mov al,carriageReturn                               ; ascii value for the carriage return: ensures that the cursor is set to the beginning of the new line
     mov ah,teletype_function                            ; bios teletype function
-    int printscreen                                     ; usdsed to print to the screen
+    int printInterrupt                                  ; usdsed to print to the screen
 
 ; reading stage 2 from the disk
-    mov ah,0x02
-    mov al,8
-    mov ch,0
-    mov cl,2
-    mov dh,0
-    mov dl,0x00
-    mov bx,0x8000
-    int 0x13
-    jc bootloader_loop  
+    mov ah,0x02                                         ; bios read sector, telling the bios to rread from the specified source
+    mov al,8                                            ; number of sections to read from the dis, each section is of 512 bytes
+    mov ch,0                                            ; specifying which ring of the disk should the data be read from
+    mov cl,2                                            ; specifying the section to be read from, sections start from 1 not 0 all the way to 63
+    mov dh,0                                            ; which plate to read from, which head to read from
+    mov dl,0x00                                         ; specifing the type of drive 0x00 is floppy disk , 0x80 is hard disk, 0x01 is floppy disk 2
+    mov bx,0x8000                                       ; read and write location, bios sees this and starts reading from this location
+    int 0x13                                            ; interrupt that lets the bios read the file from the above parameters
+    jc bootloader_loop                                  ; if failed to read the disk go to the inifnite loop instead of crashing
 
 
     mov si, stage2msg
 
-; LOOP TO PRINT THE VERIFICATION MESSGAE
-print_loop2:                                            ; loop to print the second messgae
-    lodsb                                               ; loading from si into al and moving to the nect itrem
+; LOOP TO PRINT THE VERIFICATION MESSAGE
+print_loop2:                                            ; loop to print the second message
+    lodsb                                               ; loading from si into al and moving to the next item
     cmp al,0                                            ; comparing the values of the al register with 0
     je stage2_jump                                      ; jumping to new line
     mov ah,teletype_function                            ; bios teletype function
-    int printscreen                                     ; to p[rint ht evalue in the al reguister to the screen
+    int printInterrupt                                  ; to print the value in the al register to the screen
     jmp print_loop2                                     ; repeat until the condition is met
 
 
@@ -66,10 +67,10 @@ stage2_jump:
 
 ; LOOP TO KEEP THE BOOTLOADER RUNNING 
 bootloader_loop:                                        ; bootloader loop 
-    jmp $                                               ; jumping bacck to the current memory address i.e the bootloader_loop
+    jmp $                                               ; jumping back to the current memory address i.e the bootloader_loop
 
-    stage1msg db "STAGE 1 BOOTLOADER LOADED",0          ; the first messgae to print to the screen
-    stage2msg db "STAGE 2 LOADED.", 0                   ; VERIFICATION MESSGAE AFTER STAGE 2 DISKIS read
+    stage1msg db "STAGE 1 BOOTLOADER LOADED",0          ; the first message to print to the screen
+    stage2msg db "STAGE 2 LOADED.", 0                   ; verification message after the disk is read
 
     times 510 - ( $- $$ ) db 0                          ; setting the remaining bits of the first stage to 0
     dw 0xaa55                                           ; final verification boot signature
