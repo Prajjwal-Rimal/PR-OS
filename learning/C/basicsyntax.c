d6640185825b99a63a64513df091a8688d379a7b
PR <132150362+Prajjwal-Rimal@users.noreply.github.com>
2026-04-08 13:42:36 +0545

    started learning C

---
+++ learning/C/basicsyntax.c
@@ -0,0 +1,15 @@
+// header file defines few basic functions for the program
+#include <stdio.h>
+
+// main function/the start point for the program
+int main() {
+
+  // print f is used to print lines, each statement must end with a semicolon
+  printf("Hello, World!");
+
+  // return 0 tells the program ran sucessfully any other indicates an error
+  return 0;
+}
+
+
+// to run this compile with gcc ant the output flag, then run the executable
